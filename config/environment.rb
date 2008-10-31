4e6a3739265ccf2111a019a0d4ddfdc58bd02bba
W. Andrew Loe III <andrew@andrewloe.com>
2008-10-31 14:32:08 -0700

    Modified load order to avoid rails version conflicts.

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,16 @@
+# Be sure to restart your web server when you modify this file.
+
+# Uncomment below to force Rails into production mode when 
+# you don't control web/app server and can't set it the proper way
+# ENV['RAILS_ENV'] ||= 'production'
+
+# Specifies gem version of Rails to use when vendor/rails is not present
+#RAILS_GEM_VERSION = '1.2.3' unless defined? RAILS_GEM_VERSION
+
+# Bootstrap the Rails environment, frameworks, and default configuration
+require File.join(File.dirname(__FILE__), 'boot')
+
+# Framework needs to boot first so puppet/rails uses the same version.
 require 'puppet/rails'
 module Rails
   class Configuration
@@ -25,17 +38,6 @@ module Rails
     end
   end
 end
-# Be sure to restart your web server when you modify this file.
-
-# Uncomment below to force Rails into production mode when 
-# you don't control web/app server and can't set it the proper way
-# ENV['RAILS_ENV'] ||= 'production'
-
-# Specifies gem version of Rails to use when vendor/rails is not present
-#RAILS_GEM_VERSION = '1.2.3' unless defined? RAILS_GEM_VERSION
-
-# Bootstrap the Rails environment, frameworks, and default configuration
-require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here
